0fa1d92e5c73a2751f1db1d48088dbcd8e5bd7e6
rakesh <kallurirakesh15@gmail.com>
2020-09-11 17:03:41 -0400

    Password Generator Final Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  length = 0;
   useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
   onChangeUseLetters(): void {
     this.useLetters = !this.useLetters;
   }
+  onChangeUseNumbers(): void {
+    this.useNumbers = !this.useNumbers;
+  }
+  onChangeUseSymbols(): void {
+    this.useSymbols = !this.useSymbols;
+  }
+  onChangeLength(value): void {
+    // tslint:disable-next-line:radix
+    const parseNumber = parseInt(value);
+    if (!isNaN(parseNumber)) {
+      this.length = parseNumber;
+    }
+  }
   onButtonClick(): void {
-    console.log(this.useLetters);
-    this.password = 'my password generated';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '0123456789';
+    const symbols = '!@#$%^&*()';
+
+    let characterSet = '';
+    if (this.useLetters) {
+      characterSet += letters;
+    }
+    if (this.useNumbers) {
+      characterSet += numbers;
+    }
+    if (this.useSymbols) {
+      characterSet += symbols;
+    }
+
+    let generatedPswd = '';
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * characterSet.length);
+      generatedPswd += characterSet[index];
+    }
+    this.password = generatedPswd;
   }
 }
